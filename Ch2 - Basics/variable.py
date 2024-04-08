2973be4230faf229774f456ffff29fe49226ac7e
ramses2099 <ramses2099@gmail.com>
2024-04-08 10:57:45 -0400

    first commit course learning python

---
+++ Ch2 - Basics/variable.py
@@ -0,0 +1,21 @@
+
+#
+# Example for variable in python
+#
+
+# Basic data types in Python: Numbers, Strings, Booleans, Sequences, Dictionaries
+myint = 5
+myfloat = 13.2
+mystr = "This is a string"
+mybool = True
+mylist = [0,1,"two",3.2, False]
+mytuple = (0,1,2)
+mydict = {"one":1, "two":2}
+
+print(myint)
+print(myfloat)
+print(mystr)
+print(mybool)
+print(mylist)
+print(mytuple)
+print(mydict)
